77d1a62b113d474387acc0ed8d71fccdcabf71cd
Giosuè Sulipano <sulipano.gs@gmail.com>
2022-04-26 18:23:37 +0200

    Change button text based on theme selected

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,16 @@
+const themeButton = document.getElementById("theme-button");
+
 // Check user theme preferences
 const userThemePreferences = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 switch (userThemePreferences) {
     case 'dark':
         document.documentElement.setAttribute('data-theme', userThemePreferences);
+        themeButton.value = '🌚 dark';
         break;
     case 'light':
         document.documentElement.setAttribute('data-theme', userThemePreferences);
+        themeButton.value = '🌞 light';
         break;
     case 'system':
         const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
@@ -15,6 +19,7 @@ switch (userThemePreferences) {
         } else {
             document.documentElement.setAttribute('data-theme', 'light');
         }
+        themeButton.value = '💻 system';
         break;
 }
 // TO-DO: Theme switch
