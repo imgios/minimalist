946b1188ebad2af0b3ecac41c8b7eee5d9d9bfaa
Giosuè Sulipano <sulipano.gs@gmail.com>
2022-04-26 19:08:30 +0200

    JS code refactoring

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,34 +1,36 @@
 const themeButton = document.getElementById("theme-button");
 const body = document.body;
 
+const setLightTheme = () => {
+    document.documentElement.setAttribute('data-theme', 'light');
+    body.classList.remove('text-light');
+    themeButton.classList.add('btn-outline-dark');
+    themeButton.classList.remove('btn-outline-light');
+    themeButton.innerText = '🌞 light';
+};
+
+const setDarkTheme = () => {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    body.classList.add('text-light');
+    themeButton.classList.add('btn-outline-light');
+    themeButton.classList.remove('btn-outline-dark');
+    themeButton.innerText = '🌚 dark';
+};
+
 const switchTheme = (theme) => {
     switch (theme) {
         case 'dark':
-            document.documentElement.setAttribute('data-theme', theme);
-            body.classList.add('text-light');
-            themeButton.classList.add('btn-outline-light');
-            themeButton.classList.remove('btn-outline-dark');
-            themeButton.innerText = '🌚 dark';
+            setDarkTheme();
             break;
         case 'light':
-            document.documentElement.setAttribute('data-theme', theme);
-            body.classList.remove('text-light');
-            themeButton.classList.add('btn-outline-dark');
-            themeButton.classList.remove('btn-outline-light');
-            themeButton.innerText = '🌞 light';
+            setLightTheme();
             break;
         case 'system':
-            const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+            const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
             if (prefersDarkScheme.matches) {
-                document.documentElement.setAttribute('data-theme', 'dark');
-                body.classList.add('text-light');
-                themeButton.classList.add('btn-outline-light');
-                themeButton.classList.remove('btn-outline-dark');
+                setDarkTheme();
             } else {
-                document.documentElement.setAttribute('data-theme', 'light');
-                body.classList.remove('text-light');
-                themeButton.classList.add('btn-outline-dark');
-                themeButton.classList.remove('btn-outline-light');
+                setLightTheme();
             }
             themeButton.innerText = '💻 system';
             break;
