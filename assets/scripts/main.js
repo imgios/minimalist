f7690731f9e18602b9a154c38298b7a8787497f4
Giosuè Sulipano <sulipano.gs@gmail.com>
2022-04-26 18:21:03 +0200

    Check user theme preferences

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,20 @@
 // Check user theme preferences
 const userThemePreferences = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-if (userThemePreferences) {
-    document.documentElement.setAttribute('data-theme', userThemePreferences)
+switch (userThemePreferences) {
+    case 'dark':
+        document.documentElement.setAttribute('data-theme', userThemePreferences);
+        break;
+    case 'light':
+        document.documentElement.setAttribute('data-theme', userThemePreferences);
+        break;
+    case 'system':
+        const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+        if (prefersDarkScheme.matches) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+        }
+        break;
 }
 // TO-DO: Theme switch
